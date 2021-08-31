196c19f3b27769a0fa46ab0088b358ee211bcc71
DevLuu <luu.vale.05@gmail.com>
2021-08-31 01:10:58 -0500

    inicializar proyecto

---
+++ Practica_Fork.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <windows.h>
+
+int main(int argc, char const *argv[])
+{
+    int x = 5;
+    //int who = fork();
+    x = 8;
+    printf("Hola mundo===================");
+    return 0;
+}
